d0f668ea2debba2b02ec5b212c2d222dbd3cb588
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-03 12:41:58 +0700

    coba

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    protected $table = 'wpjs_users';
     /**
      * The attributes that are mass assignable.
      *
