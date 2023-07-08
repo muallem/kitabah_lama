3f3406e0a0d611ba3f9a15de29f6ba51126f49d5
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-08 17:13:18 +0700

    logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,8 +87,7 @@
                 processData: false,
                 contentType: false,
                 success: function(response) {
-                    let data = JSON.parse(response);;
-                    console.log(data)
+                    console.log(response)
                     if(data.message !== 'success'){
                             Swal.fire({
                             type: 'error',
